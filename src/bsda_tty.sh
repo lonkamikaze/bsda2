3fecb54539f4e002d825bb4f38724574aa21d95c
Dominic Fandrey <kamikaze@bsdforen.de>
2016-06-09 19:57:01 +0200

    Add a lightweight async terminal control
    The bsda:tty:Async class supports the frequently used parts of the
    bsda:tty:Terminal interface. The only usage difference is that the
    deactivate() method needs to be called before termination.

--- src/bsda_tty.sh
+++ src/bsda_tty.sh
@@ -1,8 +1,7 @@
 test -n "$_bsda_tty_" && return 0
 readonly _bsda_tty_=1
 
-# Include framework for object oriented shell scripting.
-. ${bsda_dir:-.}/bsda_obj.sh
+. ${bsda_dir:-.}/bsda_fifo.sh
 
 #
 # A package for controlling the terminal and mixing status output on
@@ -48,6 +47,205 @@ readonly _bsda_tty_=1
 # | eat_newline_glitch | xn       |
 # | init_tabs          | it => #  |
 #
+
+#
+# Provides asynchronous terminal output.
+#
+bsda:obj:createClass bsda:tty:Async \
+	r:private:fifo      "The FIFO to communicate through" \
+	r:private:active    "Whether status line output is active" \
+	i:private:init      "Set up asynchronous output process" \
+	c:private:clean     "Terminate the output process" \
+	x:public:use        "Set number of status lines" \
+	x:public:line       "Set a status line" \
+	x:public:deactivate "Deactivate status lines" \
+	x:public:stdout     "Print to stdout" \
+	x:public:stderr     "Print to stderr"
+
+bsda:tty:Async.init() {
+	bsda:fifo:Fifo ${this}fifo
+	setvar ${this}active
+	if [ -w /dev/tty ]; then
+		setvar ${this}active 1
+		$class.daemon &
+	fi
+}
+
+bsda:tty:Async.clean() {
+	local fifo
+	$this.deactivate
+	$this.getFifo fifo
+	$fifo.delete
+}
+
+bsda:tty:Async.use() {
+	if eval "[ -z \"\$${this}active\" ]"; then
+		return
+	fi
+	$($this.getFifo).sink "printf 'use%d\n' $(($1))"
+}
+
+bsda:tty:Async.line() {
+	if eval "[ -z \"\$${this}active\" ]"; then
+		return
+	fi
+	local NL
+	NL='
+'
+	$($this.getFifo).sink "printf 'line%d=%s\n' $(($1)) '$(echo "${2%%$NL*}" | bsda:obj:escape)'"
+}
+
+bsda:tty:Async.deactivate() {
+	if eval "[ -n \"\$${this}active\" ]"; then
+		setvar ${this}active
+		$($this.getFifo).sink echo exit
+	fi
+}
+
+bsda:tty:Async.stdout() {
+	if eval "[ -n \"\$${this}active\" ]"; then
+		$($this.getFifo).sink "printf 'stdout%s\n' '$(echo "$1" | bsda:obj:escape)'"
+	else
+		echo "$1"
+	fi
+}
+
+bsda:tty:Async.stderr() {
+	if eval "[ -n \"\$${this}active\" ]"; then
+		$($this.getFifo).sink "printf 'stderr%s\n' '$(echo "$1" | bsda:obj:escape)'"
+	else
+		echo "$1" >&2
+	fi
+}
+
+#
+# @param cols
+# @param lines
+#
+bsda:tty:Async.daemon_winch() {
+	cols=$(/usr/bin/tput co 2> /dev/tty || echo 80)
+	/usr/bin/tput xn || cols=$((cols - 1))
+	lines=$(/usr/bin/tput li 2> /dev/tty || echo 24)
+}
+
+#
+# @param cols
+# @param lines
+# @param fifo
+# @param statusLines
+#
+bsda:tty:Async.daemon_startup() {
+	trap $class.daemon_deactivate EXIT
+	trap "$class.daemon_deactivate;exit" INT TERM
+	trap $class.daemon_winch WINCH
+	$class.daemon_winch
+	$this.getFifo fifo
+	statusLines=0
+	IFS='
+'
+	readonly fifo IFS
+}
+
+#
+# @param statusLines
+# @param cols
+# @param line0 line1 line...
+#
+bsda:tty:Async.daemon_drawlines() {
+	/usr/bin/tput vi cr
+	i=0
+	while [ $i -lt $((statusLines - 1)) ]; do
+		eval "printf '%.${cols}s\n' \"\$line$i\""
+		i=$((i + 1))
+	done
+	if [ $statusLines -gt 0 ]; then
+		eval "printf '%.${cols}s\r' \"\$line$i\""
+	fi
+	/usr/bin/tput $($class.daemon_repeat $((statusLines - 1)) up) ve
+}
+
+#
+# @param 1
+#	Number of repetitions
+# @param 2
+#	String to repeat
+#
+bsda:tty:Async.daemon_repeat() {
+	if [ $1 -le 0 ]; then
+		return
+	fi
+	echo "$2"
+	bsda:tty:Async.daemon_repeat $(($1 - 1)) "$2"
+}
+
+#
+# @param 1
+# @param statusLines
+# @param cols
+# @param line0 line1 line...
+#
+bsda:tty:Async.daemon_drawline() {
+	if [ $1 -ge $statusLines -o $1 -lt 0 ]; then
+		return
+	fi
+	/usr/bin/tput vi cr $($class.daemon_repeat $1 do)
+	eval "printf '%.${cols}s' \"\$line$1\""
+	/usr/bin/tput ce cr $($class.daemon_repeat $1 up) ve
+}
+
+#
+# @param statusLines
+# @param lines
+#
+bsda:tty:Async.daemon_use() {
+	statusLines=$((lines / 2 > $1 ? $1 : lines / 2))
+	/usr/bin/tput $($class.daemon_repeat $lines al) > /dev/tty
+	$class.daemon_drawlines > /dev/tty
+}
+
+#
+# @param lines
+#
+bsda:tty:Async.daemon_deactivate() {
+	/usr/bin/tput $($class.daemon_repeat $lines al) ve > /dev/tty
+}
+
+#
+# @param lines
+#
+bsda:tty:Async.daemon_stdout() {
+	/usr/bin/tput $($class.daemon_repeat $statusLines al) > /dev/tty
+	eval "echo $1"
+	$class.daemon_drawlines > /dev/tty
+}
+
+bsda:tty:Async.daemon() {
+	$class.daemon_startup
+	while $fifo.source read cmd; do
+	case "$cmd" in
+	line*)
+		eval "$cmd"
+		cmd="${cmd#line}"
+		$class.daemon_drawline ${cmd%%=*} > /dev/tty
+	;;
+	stdout*)
+		$class.daemon_stdout "${cmd#stdout}"
+	;;
+	stderr*)
+		$class.daemon_stdout "${cmd#stderr}" >&2
+	;;
+	use*)
+		$class.daemon_use "${cmd#use}"
+	;;
+	exit)
+		exit
+	;;
+	*)
+		echo "XXX $cmd"
+	;;
+	esac
+	done
+}
 
 # Terminal exceptions.
 readonly bsda_tty_ERR_TERMINAL_STATUSPROVIDER_INVALID=1
