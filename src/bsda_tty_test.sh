87d95b51dbb9efac44a36e3f3f4f9b3dae376315
Dominic Fandrey <kamikaze@bsdforen.de>
2016-06-09 20:43:20 +0200

    Remove old terminal control facilities
    Remove bsda:tty:Format and bsda:tty:Terminal, because they are too heavy
    to be of actual use.

--- src/bsda_tty_test.sh
+++ src/bsda_tty_test.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 bsda_dir="${0%${0##*/}}"
 . ${bsda_dir:-.}/bsda_tty.sh
-bsda:tty:Terminal term
+bsda:tty:Async term
 $term.use 6
 $term.line 0 "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 index=0
 set +f
+width=$(($(tput co || echo 80) - 6))
 for file in *; do
 	$term.stdout "$file"
-	$term.format status '<03:d>: <x-:s>' $index "$file"
+	status="$(printf "%03d: %${width}s" $index "$file")"
 	$term.line $((index % 5  + 1)) "$status"
 	index=$((index + 1))
 done
 sleep 1
-ls -f | grep -n '' | $term.stdout
+$term.stdout "$(ls -f)"
 sleep 1
 # Sleep to see whether status lines were preserved.
 $term.delete
