c346ab4bc1b6be73023265fa149df7e665db2ef5
Dominic Fandrey <kamikaze@bsdforen.de>
2017-02-27 21:30:16 +0100

    Add write locking to Fifo
    This change is due to very rare and difficult to reproduce errors,
    that might be caused by data corruption due to concurrent writes
    to the named pipe.

--- src/bsda_fifo.sh
+++ src/bsda_fifo.sh
@@ -23,8 +23,18 @@ readonly _bsda_fifo_=1
 #	$fifo.source read -r line
 #	echo "$line"
 #
+# Note that this consumes two file descriptors out of the small pool
+# of 7 available descriptors (this is a hard-coded limit in ASH).
+# The second descriptor is used as a write-lock facility.
+# The locking pipe is initialised with a single byte, which is consumed
+# by the sink() method before writing to the messaging pipe. Any
+# other process trying to write into the messaging pipe is suspended
+# until the sink() method completes and puts a new byte into the
+# locking pipe.
+#
 bsda:obj:createClass bsda:fifo:Fifo \
 	r:private:desc  "The I/O file descriptor number" \
+	r:private:lock  "The write lock file descriptor number" \
 	i:private:init  "Sets up the named pipe" \
 	c:private:clean "Releases the file descriptor" \
 	x:public:sink   "Use like eval to send" \
@@ -37,7 +47,7 @@ bsda:obj:createClass bsda:fifo:Fifo \
 # pipe and creates low overhead source() and sink() methods.
 #
 bsda:fifo:Fifo.init() {
-	local fifo desc
+	local fifo desc lock
 	# Create a named pipe
 	fifo="$(/usr/bin/mktemp -ut $this)" || return
 	bsda:obj:getDesc desc || return
@@ -47,10 +57,25 @@ bsda:fifo:Fifo.init() {
 	# Remove file system node for the named pipe
 	/bin/rm "$fifo"
 	setvar ${this}desc "$desc"
+
+	# Create a named pipe for the write lock
+	bsda:obj:getDesc lock || return
+	/usr/bin/mkfifo "$fifo" || return
+	# Open a file descriptor
+	eval "exec $lock<> '$fifo'"
+	# Remove file system node for the named pipe
+	/bin/rm "$fifo"
+	setvar ${this}lock "$lock"
+	# Release the lock for starters
+	echo >&$lock
+
 	# Create sink() and source() methods
 	eval "
 	$this.sink() {
+		local bsda_fifo_Fifo_lock
+		read -r bsda_fifo_Fifo_lock <&$lock
 		eval \"\$@\" >&$desc
+		echo >&$lock
 	}
 	$this.source() {
 		eval \"\$@\" <&$desc
@@ -60,13 +85,16 @@ bsda:fifo:Fifo.init() {
 #
 # Clean up behind the fifo.
 #
-# The destructor closes the file descriptor, releases it back into the
-# pool of available descriptors and removes the sink() and source()
-# methods.
+# The destructor closes the file descriptors and releases them back
+# into the pool of available descriptors.
 #
 bsda:fifo:Fifo.clean() {
 	local desc
 	$this.getDesc desc
+	eval "exec $desc<&-"
+	eval "exec $desc>&-"
+	bsda:obj:releaseDesc $desc
+	$this.getLock desc
 	eval "exec $desc<&-"
 	eval "exec $desc>&-"
 	bsda:obj:releaseDesc $desc
