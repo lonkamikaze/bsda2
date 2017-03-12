18c345fe4a1439bd855ec4bab44a7dc65b746503
Dominic Fandrey <kamikaze@bsdforen.de>
2017-03-12 18:59:49 +0100

    Add function and variable tests

--- src/bsda_test.sh
+++ src/bsda_test.sh
@@ -35,3 +35,134 @@ bsda:test:match() {
 	esac
 	return 1
 }
+
+#
+# Return the function type of the given function.
+#
+# | Type       | Description                                       |
+# |------------|---------------------------------------------------|
+# | alias      | The given function is a shell alias               |
+# | builtin    | The given function is a shell builtin             |
+# | function   | The given function is a shell function            |
+# | executable | The given function names a file system executable |
+# | none       | The given function cannot be found                |
+# | nil        | The `type` builtin returned an unsupported string |
+#
+# Note that `bash` only supports aliases in interactive mode.
+#
+# @param &1
+#	The variable to receive the function type
+# @param 2
+#	The function to determine the type of
+#
+bsda:test:type() {
+	setvar "$1" "$(
+		case "$(type "$2" 2>&1 )" in
+		"$2 is an alias "*|"$2 is aliased "*)
+			echo alias
+		;;
+		"$2 is a shell builtin")
+			echo builtin
+		;;
+		"$2 is a shell function"|"$2 is a function"*)
+			echo function
+		;;
+		"$2 is /"*)
+			echo executable
+		;;
+		*"$2: not found")
+			echo none
+		;;
+		*)
+			echo nil
+		;;
+		esac
+	)"
+}
+
+#
+# Check if the given function is an alias.
+#
+# @param 1
+#	The name of the function
+# @return
+#	Returns 0 for yes and 1 for no
+#
+bsda:test:isAlias() {
+	local type
+	bsda:test:type type "$1"
+	test "$type" = alias
+}
+
+#
+# Check if the given function is a builtin.
+#
+# @param 1
+#	The name of the function
+# @return
+#	Returns 0 for yes and 1 for no
+#
+bsda:test:isBuiltin() {
+	local type
+	bsda:test:type type "$1"
+	test "$type" = builtin
+}
+
+#
+# Check if the given function is a shell function.
+#
+# @param 1
+#	The name of the function
+# @return
+#	Returns 0 for yes and 1 for no
+#
+bsda:test:isFunction() {
+	local type
+	bsda:test:type type "$1"
+	test "$type" = function
+}
+
+#
+# Check if the given function is a file system executable.
+#
+# @param 1
+#	The name of the function
+# @return
+#	Returns 0 for yes and 1 for no
+#
+bsda:test:isExecutable() {
+	local type
+	bsda:test:type type "$1"
+	test "$type" = executable
+}
+
+#
+# Check if the given function cannot be found.
+#
+# @param 1
+#	The name of the function
+# @return
+#	Returns 0 for yes and 1 for no
+#
+bsda:test:isNone() {
+	local type
+	bsda:test:type type "$1"
+	test "$type" = none
+}
+
+#
+# Check if the given variable exists.
+#
+# Note that this returns true for all defined variables, even empty
+# ones.
+#
+# @param 1
+#	The name of the variable
+# @retval 0
+#	The variable has been defined
+# @retval 1
+#	The variable does not exist
+#
+bsda:test:isSet() {
+	eval "test -n \"\${$1+x}\""
+}
