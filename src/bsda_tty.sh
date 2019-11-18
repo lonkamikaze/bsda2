8507361ce5dc0474efa20d1e5ddc8b93f1dc5b9d
Dominic Fandrey <kamikaze@bsdforen.de>
2019-11-18 13:47:44 +0100

    Reimplement bsda:tty:Async
    Make bsda:tty:Async a wrapper around bsda:tty:Terminal.

--- src/bsda_tty.sh
+++ src/bsda_tty.sh
@@ -254,7 +254,6 @@ bsda:tty:Terminal.refresh() {
 #
 bsda:obj:createClass bsda:tty:Async \
 	a:private:Fifo=bsda:fifo:Fifo \
-	r:private:active    "Whether status line output is active" \
 	r:private:dpid      "The PID of the daemon process" \
 	i:private:init      "Set up asynchronous output process" \
 	c:private:clean     "Terminate the output process" \
@@ -267,23 +266,26 @@ bsda:obj:createClass bsda:tty:Async \
 #
 # Setup terminal control daemon.
 #
-# Sets up a bsda:tty:Async.daemon() instance that controls the terminal
+# Sets up a bsda:tty:Async:daemon() instance that controls the terminal
 # and receives commands through a fifo.
 #
 bsda:tty:Async.init() {
-	if [ -w /dev/tty ]; then
-		bsda:fifo:Fifo ${this}Fifo || return $?
-		setvar ${this}active 1
-		$class.daemon &
-		setvar ${this}dpid $!
-	fi
+	bsda:fifo:Fifo ${this}Fifo || return $?
+	$class:daemon &
+	setvar ${this}dpid $!
 }
 
 #
-# Deactivate the daemon and remove the fifo.
+# Harvest the terminal control daemon.
 #
 bsda:tty:Async.clean() {
-	$this.deactivate
+	local dpid
+	# Harvest the background daemon
+	$this.getDpid dpid
+	if [ -n "$dpid" ]; then
+		kill $dpid 2>&-
+		wait $dpid || :
+	fi
 }
 
 #
@@ -293,9 +295,6 @@ bsda:tty:Async.clean() {
 #	The number of lines
 #
 bsda:tty:Async.use() {
-	if eval "[ -z \"\$${this}active\" ]"; then
-		return
-	fi
 	$($this.Fifo).sink "printf 'use %d\n' $(($1))"
 }
 
@@ -309,42 +308,26 @@ bsda:tty:Async.use() {
 #
 bsda:tty:Async.line() {
 	# Redefine into minimal function
-	if eval "[ -n \"\$${this}active\" ]"; then
-		eval "$this.line() {
-			local NL str line
-			NL=$'\n'
-			str=\"\$(echo -n \"\${2%%\$NL*}\" | bsda:obj:escape)\"
-			line=\$((\$1))
-			$($this.Fifo).sink 'echo \"line\$line \$str\"'
-		}"
-	else
-		eval "$this.line() {
-			:
-		}"
-	fi
+	eval "$this.line() {
+		local NL str line
+		NL=$'\n'
+		str=\"\$(echo -n \"\${2%%\$NL*}\" | bsda:obj:escape)\"
+		line=\$((\$1))
+		$($this.Fifo).sink 'echo \"line\$line \$str\"'
+	}"
 	# Call the redefined function
-	if [ -n "$*" ]; then
-		$this.line "$@"
-	fi
+	$this.line "$@"
 }
 
 #
-# Disable status output and stop the daemon.
+# Disable status output.
 #
-# Deactivating the daemon is not reversible, but the stdout() and
-# stderr() methods still produce synchronous output.
+# The stdout() and # stderr() methods still produce output.
 #
 bsda:tty:Async.deactivate() {
-	if eval "[ -n \"\$${this}active\" ]"; then
-		setvar ${this}active
-		$($this.Fifo).sink echo exit
-		# Reset self-optimising functions
-		bsda:tty:Async.line
-		bsda:tty:Async.stdout
-		bsda:tty:Async.stderr
-		# Wait for daemon to exit
-		wait $($this.getDpid)
-	fi
+	local fifo
+	$this.Fifo fifo
+	$fifo.sink echo deactivate
 }
 
 #
@@ -358,21 +341,13 @@ bsda:tty:Async.deactivate() {
 #
 bsda:tty:Async.stdout() {
 	# Redefine into minimal function
-	if eval "[ -n \"\$${this}active\" ]"; then
-		eval "$this.stdout() {
-			local str
-			str=\"\$(echo \"\$*\" | bsda:obj:escape)\"
-			$($this.Fifo).sink 'echo \"stdout \$str\"'
-		}"
-	else
-		eval "$this.stdout() {
-			echo \"\$*\"
-		}"
-	fi
+	eval "$this.stdout() {
+		local str
+		str=\"\$(echo \"\$*\" | bsda:obj:escape)\"
+		$($this.Fifo).sink 'echo \"stdout \$str\"'
+	}"
 	# Call the redefined function
-	if [ -n "$*" ]; then
-		$this.stdout "$@"
-	fi
+	$this.stdout "$@"
 }
 
 #
@@ -385,180 +360,13 @@ bsda:tty:Async.stdout() {
 #	The strings to output
 #
 bsda:tty:Async.stderr() {
-	if eval "[ -n \"\$${this}active\" ]"; then
-		eval "$this.stderr() {
-			local str
-			str=\"\$(echo \"\$*\" | bsda:obj:escape)\"
-			$($this.Fifo).sink 'echo \"stderr \$str\"'
-		}"
-	else
-		eval "$this.stderr() {
-			echo \"\$*\"
-		}"
-	fi
+	eval "$this.stderr() {
+		local str
+		str=\"\$(echo \"\$*\" | bsda:obj:escape)\"
+		$($this.Fifo).sink 'echo \"stderr \$str\"'
+	}"
 	# Call the redefined function
-	if [ -n "$*" ]; then
-		$this.stderr "$@"
-	fi
-}
-
-#
-# Updates the number of terminal columns and lines.
-#
-# This is called during daemon startup and use() is called.
-#
-# @param lines
-#	Set to the available number of lines
-# @param drawLines
-#	The number of status lines to actually draw
-# @param statusLines
-#	The number of status lines requested
-#
-bsda:tty:Async.daemon_winch() {
-	lines=$(/usr/bin/tput li 2> /dev/tty || echo 24)
-	# Use at most half of the available terminal space
-	drawLines=$((statusLines < (lines / 2) ? statusLines : (lines / 2)))
-}
-
-#
-# Initialises globals required in the daemon process.
-#
-# @param lines
-#	See bsda:tty:Async.daemon_winch()
-# @param drawLines
-#	See bsda:tty:Async.daemon_winch()
-# @param fifo
-#	The FIFO the commands are coming from
-# @param statusLines
-#	The initial amount of status lines (0)
-# @param IFS
-#	Set to the newline
-# @param trapped
-#	Initially empty
-#
-bsda:tty:Async.daemon_startup() {
-	trap "$class.daemon_deactivate" EXIT
-	$class.daemon_winch
-	$this.Fifo fifo
-	# Handle signals
-	trap "trap '' HUP INT TERM;$fifo.sink echo exit" HUP INT TERM
-	trap "trap '' WINCH;$fifo.sink echo winch" WINCH
-	statusLines=0
-	IFS=$' \t\n'
-	readonly fifo IFS
-}
-
-#
-# Draw all the status lines.
-#
-# The caller is responsible for redirecting the output to /dev/tty.
-#
-# @param drawLines
-#	The number of status lines to draw
-# @param line0 line1 line...
-#	The status line buffers
-#
-bsda:tty:Async.daemon_drawlines() {
-	if [ $((drawLines)) -le 0 ]; then
-		return 0
-	fi
-	local i
-	i=$((drawLines - 1))
-	# tput       vi.......                                     RA......
-	printf '%b' '\033[?25l\r' $($class.daemon_repeat $i '\n') '\033[?7l'
-	while [ $i -gt 0 ]; do
-		# tput          ce....  up...
-		eval "printf '%s\033[K\r\033M' \"\$line$i\""
-		i=$((i - 1))
-	done
-	# tput    ce....  SA......ve...............
-	printf '%s\033[K\r\033[?7h\033[34h\033[?25h' "$line0"
-}
-
-#
-# Prints a string a given number of times.
-#
-# This can be used to generate repeated function arguments.
-#
-# @param 1
-#	Number of repetitions
-# @param 2
-#	String to repeat
-#
-bsda:tty:Async.daemon_repeat() {
-	if [ $1 -le 0 ]; then
-		return
-	fi
-	echo "$2"
-	bsda:tty:Async.daemon_repeat $(($1 - 1)) "$2"
-}
-
-#
-# Draw the given status line.
-#
-# This jumps to the given line and redraws it from the buffer.
-#
-# @param 1
-#	The status line number to draw on
-# @param drawLines
-#	The number of status lines that may be drawn
-# @param line0 line1 line...
-#	The status line buffers
-#
-bsda:tty:Async.daemon_drawline() {
-	if [ $1 -ge $drawLines ] || [ $1 -lt 0 ]; then
-		return
-	fi
-	# tput       vi.......
-	printf '%b' '\033[?25l\r' $($class.daemon_repeat $1 '\n')
-	eval "printf '\033[?7l%s\033[K\r\033[?7h' \"\$line$1\""
-	# tput                                 up...    ve...............
-	printf '%b' $($class.daemon_repeat $1 '\033M') '\033[34h\033[?25h'
-}
-
-#
-# Changes the number of status lines.
-#
-# Updates the number of desired status lines, sets the number of
-# actual status lines to draw and redraws the status.
-#
-# @param 1
-#	The requested number of status lines
-# @param statusLines
-#	Set to the requested number
-# @param drawLines
-#	See bsda:tty:Async.daemon_winch()
-#
-bsda:tty:Async.daemon_use() {
-	statusLines=$(($1))
-	# Update drawLines
-	$class.daemon_winch
-}
-
-#
-# Cleanup function.
-#
-# Clear the status lines, and turn the cursor visible.
-#
-bsda:tty:Async.daemon_deactivate() {
-	# tput      cd..ve...........
-	echo -n $'\r\e[J\e[34h\e[?25h' > /dev/tty
-}
-
-#
-# Print on stdout.
-#
-# Clears the status lines, prints the requested output and redraws the
-# status lines.
-#
-# @param 1
-#	A quoted, escaped string, such as bsda:obj:escape() produces
-#
-bsda:tty:Async.daemon_stdout() {
-	# tput    cd..
-	echo -n $'\e[J' > /dev/tty
-	echo "$1"
-	$class.daemon_drawlines > /dev/tty
+	$this.stderr "$@"
 }
 
 #
@@ -566,8 +374,11 @@ bsda:tty:Async.daemon_stdout() {
 #
 # Receives and executes commands from the daemon.
 #
-bsda:tty:Async.daemon() {
-	$class.daemon_startup
+bsda:tty:Async:daemon() {
+	local fifo term cmd arg retval
+	bsda:obj:fork
+	$this.Fifo fifo
+	bsda:tty:Terminal term
 	while true; do
 		$fifo.source read -r cmd arg
 		retval=$?
@@ -575,36 +386,19 @@ bsda:tty:Async.daemon() {
 			# Retry read if interrupted by a signal
 			continue
 		elif [ $retval -ne 0 ]; then
-			$class.daemon_stdout "bsda:tty:Async.daemon: ERROR: Read from pipe returned: $retval" >&2
+			$term.stderr "bsda:tty:Async.daemon: ERROR: Read from pipe returned: $retval"
 			exit $retval
 		fi
 		arg="$(printf "$arg")" # Deserialise
 		case "$cmd" in
-		line*)
-			setvar "$cmd" "$arg"
-			$class.daemon_drawline ${cmd#line} > /dev/tty
+		line[0-9]*)
+			$term.${cmd%%[0-9]*} ${cmd#${cmd%%[0-9]*}} "$arg"
 		;;
-		stdout)
-			$class.daemon_stdout "$arg"
-		;;
-		stderr)
-			$class.daemon_stdout "$arg" >&2
-		;;
-		use)
-			$class.daemon_use "$arg"
-		;;
-		exit)
-			exit 0
-		;;
-		winch)
-			trap "trap '' WINCH;$fifo.sink echo winch" WINCH
-			$class.daemon_winch
-		;;
-		'')
-			# This seems to happen on SIGINT/SIGHUP/SIGTERM
+		stdout | stderr | use | deactivate)
+			$term.$cmd "$arg"
 		;;
 		*)
-			$class.daemon_stdout "bsda:tty:Async.daemon: WARNING: Illegal command received: $(echo -n "$cmd" | bsda:obj:escape)" >&2
+			$term.stderr "bsda:tty:Async.daemon: WARNING: Illegal command received: $(echo -n "$cmd" | bsda:obj:escape)"
 		;;
 		esac
 	done
