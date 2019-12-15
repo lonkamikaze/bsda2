00f9e575526b6d3fe5a93df999c50fead896afd4
Dominic Fandrey <kamikaze@bsdforen.de>
2019-12-15 14:08:27 +0100

    Add tests for flag initialisation

--- tests/bsda_opts.sh
+++ tests/bsda_opts.sh
@@ -112,3 +112,183 @@ $flags.check FOO -eq 2
 
 # Clear up
 $flags.delete
+
+# Check Flags initialisation from environment
+unset FOO BAR BAZ
+FOO=2
+BAR=1
+BAZ=1
+
+# Do not use environment
+bsda:opts:Flags flags
+$flags.check FOO -eq 0
+$flags.check BAR -eq 0
+$flags.check BAZ -eq 0
+$flags.delete
+
+# Use environment, but variables are not available
+bsda:opts:Flags flags FOO BAR BAZ
+$flags.check FOO -eq 0
+$flags.check BAR -eq 0
+$flags.check BAZ -eq 0
+$flags.delete
+
+# Add variables to environment
+export FOO BAR BAZ
+
+# Do not use environment
+bsda:opts:Flags flags
+$flags.check FOO -eq 0
+$flags.check BAR -eq 0
+$flags.check BAZ -eq 0
+$flags.delete
+
+# Use environment
+bsda:opts:Flags flags FOO BAR BAZ
+$flags.check FOO -eq 2
+$flags.check BAR -eq 1
+$flags.check BAZ -eq 1
+$flags.delete
+
+# Ignore invalid values
+BAR=-1
+BAZ=firecracker
+bsda:opts:Flags flags FOO BAR BAZ
+$flags.check FOO -eq 2
+$flags.check BAR -eq 0
+$flags.check BAZ -eq 0
+$flags.delete
+
+# Assign regardless of env
+bsda:opts:Flags flags FOO=3 BAR=4 BAZ=5
+$flags.check FOO -eq 3
+$flags.check BAR -eq 4
+$flags.check BAZ -eq 5
+$flags.delete
+
+# Assign regardless of env, ignoring invalid values
+BAR=1
+BAZ=1
+bsda:opts:Flags flags FOO=3 BAR=-1 BAZ=firecracker
+$flags.check FOO -eq 3
+$flags.check BAR -eq 0
+$flags.check BAZ -eq 0
+$flags.delete
+
+# Assign default values, env has higher priority
+bsda:opts:Flags flags FOO?=3 BAR?=4 BAZ?=5
+$flags.check FOO -eq 2
+$flags.check BAR -eq 1
+$flags.check BAZ -eq 1
+$flags.delete
+
+# Assign default values, env has higher priority
+unset BAR  # not in env, select default
+BAZ=       # empty string > default
+bsda:opts:Flags flags FOO?=3 BAR?=4 BAZ?=5
+$flags.check FOO -eq 2
+$flags.check BAR -eq 4
+$flags.check BAZ -eq 0
+$flags.delete
+
+# Test boolean values
+unset A B C D E F G H I J K L M N O
+bsda:opts:Flags flags A= B=0 C=no D=NO E=No F=false G=FALSE H=False \
+                      I=1 J=yes K=YES L=Yes M=true N=TRUE O=True
+$flags.check A -eq 0
+$flags.check B -eq 0
+$flags.check C -eq 0
+$flags.check D -eq 0
+$flags.check E -eq 0
+$flags.check F -eq 0
+$flags.check G -eq 0
+$flags.check H -eq 0
+$flags.check I -eq 1
+$flags.check J -eq 1
+$flags.check K -eq 1
+$flags.check L -eq 1
+$flags.check M -eq 1
+$flags.check N -eq 1
+$flags.check O -eq 1
+$flags.delete
+
+# Test boolean default values
+bsda:opts:Flags flags A?= B?=0 C?=no D?=NO E?=No F?=false G?=FALSE H?=False \
+                      I?=1 J?=yes K?=YES L?=Yes M?=true N?=TRUE O?=True
+$flags.check A -eq 0
+$flags.check B -eq 0
+$flags.check C -eq 0
+$flags.check D -eq 0
+$flags.check E -eq 0
+$flags.check F -eq 0
+$flags.check G -eq 0
+$flags.check H -eq 0
+$flags.check I -eq 1
+$flags.check J -eq 1
+$flags.check K -eq 1
+$flags.check L -eq 1
+$flags.check M -eq 1
+$flags.check N -eq 1
+$flags.check O -eq 1
+$flags.delete
+
+# Set boolean values from environment
+export A=1 B=yes C=YES D=Yes E=true F=TRUE G=True \
+       H= I=0 J=no K=NO L=No M=false N=FALSE O=False
+bsda:opts:Flags flags A B C D E F G H I J K L M N O
+$flags.check A -eq 1
+$flags.check B -eq 1
+$flags.check C -eq 1
+$flags.check D -eq 1
+$flags.check E -eq 1
+$flags.check F -eq 1
+$flags.check G -eq 1
+$flags.check H -eq 0
+$flags.check I -eq 0
+$flags.check J -eq 0
+$flags.check K -eq 0
+$flags.check L -eq 0
+$flags.check M -eq 0
+$flags.check N -eq 0
+$flags.check O -eq 0
+$flags.delete
+
+# Override boolean defaults from environment
+bsda:opts:Flags flags A?= B?=0 C?=no D?=NO E?=No F?=false G?=FALSE H?=False \
+                      I?=1 J?=yes K?=YES L?=Yes M?=true N?=TRUE O?=True
+$flags.check A -eq 1
+$flags.check B -eq 1
+$flags.check C -eq 1
+$flags.check D -eq 1
+$flags.check E -eq 1
+$flags.check F -eq 1
+$flags.check G -eq 1
+$flags.check H -eq 0
+$flags.check I -eq 0
+$flags.check J -eq 0
+$flags.check K -eq 0
+$flags.check L -eq 0
+$flags.check M -eq 0
+$flags.check N -eq 0
+$flags.check O -eq 0
+$flags.delete
+
+# Set boolean values regardless of environment
+bsda:opts:Flags flags A= B=0 C=no D=NO E=No F=false G=FALSE H=False \
+                      I=1 J=yes K=YES L=Yes M=true N=TRUE O=True
+$flags.check A -eq 0
+$flags.check B -eq 0
+$flags.check C -eq 0
+$flags.check D -eq 0
+$flags.check E -eq 0
+$flags.check F -eq 0
+$flags.check G -eq 0
+$flags.check H -eq 0
+$flags.check I -eq 1
+$flags.check J -eq 1
+$flags.check K -eq 1
+$flags.check L -eq 1
+$flags.check M -eq 1
+$flags.check N -eq 1
+$flags.check O -eq 1
+$flags.delete
